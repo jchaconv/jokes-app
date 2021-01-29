10ddf4cb57ed0278745df5e5c3e75adddf27f785
jchaconv <jcaaron.chacon@outlook.com>
2021-01-28 22:52:57 -0500

    added files to the project

---
+++ src/main/java/expert/springframework/joke/JokesappApplication.java
@@ -0,0 +1,13 @@
+package expert.springframework.joke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokesappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/expert/springframework/joke/JokesappApplicationTests.java
@@ -0,0 +1,13 @@
+package expert.springframework.joke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
